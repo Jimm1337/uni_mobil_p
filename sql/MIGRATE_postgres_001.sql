60c8960e08787d4ed70303b587ebf26bd2ebd832
Jimm1337 <263970@student.pwr.edu.pl>
2024-06-19 03:11:11 +0200

    [Bugfix] Fix db for linux.

--- sql/MIGRATE_postgres_001.sql
+++ sql/MIGRATE_postgres_001.sql
@@ -41,8 +41,8 @@ BEGIN
                   WITH
                   OWNER = "Supervisor"
                   ENCODING = ''UTF8''
-                  LC_COLLATE = ''Polish_Poland.1250''
-                  LC_CTYPE = ''Polish_Poland.1250''
+                  LC_COLLATE = ''C.UTF-8''
+                  LC_CTYPE = ''C.UTF-8''
                   TABLESPACE = pg_default
                   CONNECTION LIMIT = -1');
    END IF;
